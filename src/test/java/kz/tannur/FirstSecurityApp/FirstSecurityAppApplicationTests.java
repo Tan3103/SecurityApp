540066dbdd40047f7e1676c57ed493c25e8221b6
Tan3103 <tannur.uataev@mail.ru>
2023-03-28 22:00:01 +0600

    first

---
+++ src/test/java/kz/tannur/FirstSecurityApp/FirstSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.tannur.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
